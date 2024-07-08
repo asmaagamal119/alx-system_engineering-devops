e93e5a3ccc0afacf379faa38cd26c68ac0efd240
asmaagamal119 <asmaagamal1109@gmail.com>
2024-07-08 13:59:42 -0700

    add

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,21 @@
-# Automation: creates a custom HTTP header
-exec { 'command':
-  command  => 'apt-get -y update;
-  apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-  service nginx restart',
-  provider => shell,
+# Install and configure `Nginx` web server to have a custome header
+exec { 'apt_update':
+  command => 'apt-get update',
+  path    => '/usr/bin:/bin',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt_update'],
+}
+
+file_line { 'add_custom_header':
+  path  => '/etc/nginx/sites-available/default',
+  line  => "\n\tadd_header X-Served-By ${hostname};",
+  after => 'server_name _;',
+}
+
+exec { 'restart_nginx':
+  command => 'sudo service nginx restart',
+  path    => '/usr/bin:/bin',
 }
--- 0x0F-load_balancer/README.md
+++ 0x0F-load_balancer/README.md
@@ -1,1 +1,1 @@
-this is my readme.md file
+this is my README.md file
