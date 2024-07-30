d9262e80cbc5df750e4511d7a897944786b75bd7
asmaagamal119 <asmaagamal109@gmail.com>
2024-07-30 02:37:58 +0000

    add

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,38 @@
+#!/usr/bin/python3
+"""For a given employee ID, returns information about
+their TODO list progress"""
 
-rts to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+
+    name = user.json().get('username')
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    totalTasks = 0
+    completed = 0
+    csv_file_name = "{}.csv".format(userId)
+    csv_header = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+
+    with open(csv_file_name, mode='w', newline='') as csv_file:
+        task_writer = csv.writer(csv_file, delimiter=',',
+                                 quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in todos.json():
+            if task.get('userId') == int(userId):
+                totalTasks += 1
+                if task.get('completed'):
+                    completed += 1
+                task_writer.writerow([userId, name, task.get('completed'),
+                                      task.get('title')])
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, completed, totalTasks))
+
+    print('\n'.join(["\t " + task.get('title') for task in todos.json()
+          if task.get('userId') == int(userId) and task.get('completed')]))
